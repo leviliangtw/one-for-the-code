30ceafd416ba263e23f9456a2b2a0c1058620664
Jih-Wei Liang <jihwei.liang@tum.de>
2022-11-18 11:13:14 +0100

    feature: Refactor main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,13 +20,23 @@ const Index = () => {
         />
         <BasicTextModule
           title="Cloud Computing and Cyber Security have become the trend around the world"
-          content="Using modern programming languages such as Golang, and combining cloud engineering technologies, kinds of projects are developed for innovation and convenience. Giving excellent experience on varieties of platforms."
-          link="/projects"
-          linkText="View Projects"
+          content="During developing projects in languages like C++11, Golang, and Python, plus cloud engineering technologies, pits and falls come and bring about trouble including security issues."
+          link="/blogs"
+          linkText="View My Blogs"
+        />
+        <LatestPosts
+          title="The Latest Posts"
+          introduction="Recording and sharing are quite significant and precious. No matter what they are, knowledge and experiences could help others who are blocked.."
         />
         {/* <PerksModule>
           <Perk title="The Title" content="The content" />
         </PerksModule> */}
+        {/* <BasicTextModule
+          title="Cloud Computing and Cyber Security have become the trend around the world"
+          content="Using modern programming languages such as Golang, and combining cloud engineering technologies, kinds of projects are developed for innovation and convenience. Giving excellent experience on varieties of platforms."
+          link="/projects"
+          linkText="View Projects"
+        /> */}
         <Projects
           title="Popular Projects"
           introduction="Featuring useful and powerful functions in different areas, these projects are under well development and maintenance. Aiming to provide great features for anybody, all the projects are developed with an explicit and specific goal. Please feel free to check them out for more information!"
@@ -35,10 +45,6 @@ const Index = () => {
           title="Featured Products from Barcadia."
           introduction="Vivamus quam mauris, pulvinar vel mauris id, interdum semper neque. Proin malesuada libero eget tellus scelerisque, id egestas tortor egestas."
         /> */}
-        <LatestPosts
-          title="The Latest Posts"
-          introduction="Recording and sharing of knowledge are quite significant and precious. Though the information might be very simple, the experience still could help others who are blocked or stuck here. "
-        />
       </Layout>
     </>
   )
